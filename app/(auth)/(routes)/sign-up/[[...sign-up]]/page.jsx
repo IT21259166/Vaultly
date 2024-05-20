864bfd4f490c0828e5b1ec5ce6db2790dfc3f157
JackRipper406 <it21259166@my.sliit.lk>
2024-05-21 02:55:30 +0530

    sign up commit

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,32 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
  
 export default function Page() {
-  return <SignUp />;
+  return (
+<section className="bg-white">
+  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+    <section
+      className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+    >
+      <img
+        alt="Night"
+        src="https://wallpapercave.com/wp/1PFHyYy.jpg"
+        className="absolute inset-0 h-full w-full object-cover opacity-80"
+      />
+
+      <div className="hidden lg:relative lg:block lg:p-12">
+          <Image className="mt-2 mb-96" src='/logo.svg' width={700} alt="logo" height={1200} />
+      </div>
+    </section>
+
+    <main
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+    >
+      <div className="max-w-xl lg:max-w-3xl">
+    <SignUp/>
+  </div>
+    </main>
+  </div>
+</section>
+  );
 }
