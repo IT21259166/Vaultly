e8d911dbf3bf07680ed9cefd865af3d3422d3397
JackRipper406 <it21259166@my.sliit.lk>
2024-05-22 14:41:31 +0530

    final commit

--- app/(dashboard)/(routes)/files/page.js
+++ app/(dashboard)/(routes)/files/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import { UserButton, useUser } from '@clerk/nextjs';
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useEffect, useState } from 'react';
 import UploadForm from './_components/UploadForm';
 import { app } from './../../../../firebaseConfig';
